bcf75fdb983b93aab9701395e2a1bd371e6a8763
sabilafirdias <sabilafirdias@gmail.com>
2025-11-07 12:36:49 +0700

    membuat isian body tampilan data

--- app/src/main/java/com/example/prak6/view/TampilData.kt
+++ app/src/main/java/com/example/prak6/view/TampilData.kt
@@ -41,7 +41,26 @@ fun TampilData(
                 title = {Text(text = stringResource(id=R.string.tampil), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.ungu2))
             )
-        }){}
+        }){ isiRuang->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy (10.dp)) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = colorResource(id=R.color.ungu3))
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id = R.string.back))
+                }
+            }
+        }
 
     }
 }
