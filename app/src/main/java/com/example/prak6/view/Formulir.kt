d7fa30f905db443c6f1800dd41b232db38bc982a
sabilafirdias <sabilafirdias@gmail.com>
2025-11-07 10:25:42 +0700

    membuat divider

--- app/src/main/java/com/example/prak6/view/Formulir.kt
+++ app/src/main/java/com/example/prak6/view/Formulir.kt
@@ -68,5 +68,12 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
 
+    }
 }
