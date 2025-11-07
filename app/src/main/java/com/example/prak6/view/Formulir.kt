180786f5d01b54f1247ceeca24090d0ea6785888
sabilafirdias <sabilafirdias@gmail.com>
2025-11-07 10:25:22 +0700

    membuat row untuk radio button

--- app/src/main/java/com/example/prak6/view/Formulir.kt
+++ app/src/main/java/com/example/prak6/view/Formulir.kt
@@ -56,5 +56,17 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(all = 20.dp)
                 .width(250.dp), thickness = Thickness,color = Color.Red)
+            Row{
+                jenisK.forEach{
+                    item->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
 
 }
