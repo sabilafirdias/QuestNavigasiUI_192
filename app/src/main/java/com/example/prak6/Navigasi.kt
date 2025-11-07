2ef064028b609c459baa0d1bb3517f61cb6da54a
sabilafirdias <sabilafirdias@gmail.com>
2025-11-07 12:37:38 +0700

    membuat enum class navigasi

---
+++ app/src/main/java/com/example/prak6/Navigasi.kt
@@ -0,0 +1,14 @@
+package com.example.prak6
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+
+}
