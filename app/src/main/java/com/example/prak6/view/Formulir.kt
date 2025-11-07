ed02c00e9ae2b537d6bf03ad5afcac1dd0368671
sabilafirdias <sabilafirdias@gmail.com>
2025-11-07 10:24:38 +0700

    membuat text field

--- app/src/main/java/com/example/prak6/view/Formulir.kt
+++ app/src/main/java/com/example/prak6/view/Formulir.kt
@@ -44,5 +44,14 @@ fun FormIsian(
         Column(modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top=20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
 
 }
